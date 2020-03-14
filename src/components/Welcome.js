2f8b19b234eb064a352f25ff1f538e59267d441c
Teodor Nilseng Danielsen <teodor.danielsen@gmail.com>
2020-03-14 22:11:01 +0100

    adjusting height to actual inner height

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import "../styles/Welcome.css";
@@ -6,16 +6,30 @@ import "../styles/common/buttonLight.css";
 import Footer from "./Footer";
 import AnimatedLogo from "./AnimatedLogo";
 
+const useWindowHeight = () => {
+  const [height, setHeight] = useState(0);
+  useEffect(() => {
+    function updateHeight() {
+      setHeight(window.innerHeight);
+    }
+    window.addEventListener("resize", updateHeight);
+    updateHeight();
+    return () => window.removeEventListener("resize", updateHeight);
+  }, []);
+  return height;
+};
+
 const Welcome = () => {
+  const height = useWindowHeight();
   return (
     <header>
       <AnimatedLogo />
-      <div className="topBox">
+      <div className="topBox" style={{ minHeight: height / 2 }}>
         <Link to="/getstarted">
           <button className="buttonLight">Get started</button>
         </Link>
       </div>
-      <div className="bottomBox">
+      <div className="bottomBox" style={{ minHeight: height / 2 }}>
         <button className="buttonDark">Login</button>
         <Footer />
       </div>
