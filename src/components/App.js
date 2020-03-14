5bf600a2540bb56806987a5bc53c4f22ff338209
Teodor Nilseng Danielsen <teodor.danielsen@gmail.com>
2020-03-14 18:42:00 +0100

    added routing and not found page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "../styles/App.css";
 import Welcome from "./Welcome";
+import NotFoundPage from "./NotFoundPage";
 
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      <Switch>
+        <Route path="/" component={Welcome} exact />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "40vh",
+        fontSize: "2rem",
+        fontWeight: 200
+      }}
+    >
+      Sorry, can't find the page :´(
+    </div>
+  );
+};
+
+export default NotFoundPage;
