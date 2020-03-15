1eb803ed8c5fa095de756e96a4dfd9695c89676a
Teodor Nilseng Danielsen <teodor.danielsen@gmail.com>
2020-03-15 16:53:02 +0100

    removing footer

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import "../styles/Welcome.css";
 import "../styles/common/buttonLight.css";
 import "../styles/common/buttonDark.css";
-import Footer from "./Footer";
 import AnimatedLogo from "./AnimatedLogo";
 
 const Welcome = () => {
@@ -23,7 +22,6 @@ const Welcome = () => {
           Login
         </button>
       </div>
-      <Footer />
     </header>
   );
 };
